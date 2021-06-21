1faeb1b876a638e37cddfc27844c92aca3d1e528
marcosdarioaranda <markosdarioaranda@gmail.com>
2021-06-21 10:18:27 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,76 @@
+require('colors');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+//const { mostrarMenu,pausa } = require('./helpers/mensajes');
+const { 
+    inquireMenu, 
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoCheckList  
+} = require('./helpers/inquirer');
+//const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
+console.clear();
+
+
+const main= async () =>{
+  
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareaDB = leerDB();
+
+    if (tareaDB){ //Cargar Tareas
+        tareas.cargarTareasFromArray(tareaDB);
+    }
+    
+
+    do{
+        opt =  await inquireMenu();
+        
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Desripción:');
+                tareas.crearTarea(desc);
+            break;
+            case '2':
+               tareas.listadoCompleto();
+                
+            break;
+            case '3':
+                tareas.listarPendientesCompletadas (true);
+            break;
+            case '4':
+                tareas.listarPendientesCompletadas (false);
+             break;
+             case '5':
+                const ids = await mostrarListadoCheckList(tareas.listadoArr);
+                tareas.toggleCompletadas(ids);
+             break;
+            case '6':
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if (id !== '0'){
+                    const ok = await confirmar('¿ Está seguro?\n La tarea de perdera definitivamente');
+                    if (ok){
+                        tareas.borrarTarea(id);
+                        console.log('Tarea borrada exitosamente');
+                    }
+                }
+                
+              
+            break;
+        
+        }
+        guardarDB(tareas.listadoArr);
+        /*const tareas = new Tareas();
+        const tarea = new Tarea ('Comprar comida');
+        tareas._listado[tarea.id] = tarea;
+        console.log(tareas);*/
+        await pausa();
+    }while (opt !== '0');
+    
+
+}
+
+main();
---
+++ helpers/mensajes.js
@@ -0,0 +1,63 @@
+const { resolve } = require('path');
+
+require('colors');
+
+
+const mostrarMenu = () => {
+
+    return new Promise(resolve =>{
+        console.clear();
+        console.log('==========================='.green);
+        console.log('   Seleccione una opción   ');
+        console.log('===========================\n'.green);
+    
+        console.log(`${'1.'.green} Crear Tarea`);
+        console.log(`${'2.'.green} Listar Tareas`);
+        console.log(`${'3.'.green} Listar Tareas Completadas`);
+        console.log(`${'4.'.green} Listar Tareas Pendientes`);
+        console.log(`${'5.'.green} Completar Tareas (s)`);
+        console.log(`${'6.'.green} Borrar Tareas`);
+        console.log(`${'0.'.green} Salir\n`);
+    
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question('Seleccione una opción: ',(opt)=>{
+            readline.close();
+            resolve(opt);
+    
+        })
+        
+
+    });
+    
+    
+    
+
+}
+
+const pausa = () =>{
+    return new Promise(resolve =>{
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question(`\nPresione ${ 'ENTER'.blue} para continuar\n`,(opt)=>{
+           readline.close();
+           resolve();
+        })
+
+    });
+    
+}   
+
+
+
+module.exports = {
+
+    mostrarMenu,
+    pausa
+}
